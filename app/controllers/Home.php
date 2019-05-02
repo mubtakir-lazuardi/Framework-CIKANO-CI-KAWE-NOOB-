3aa5c6ef986288c037ab9ee6c6810bb4508c90f3
Your Name <you@example.com>
2019-05-02 13:58:18 +0700

    model revisi

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,8 +3,9 @@
 class Home extends Controller{
     public function index(){
         $data['judul'] = 'Home';
+        $data['nama']  = $this->model('User_model')->getUser();
         $this->view('templates/header', $data);
-        $this->view('home/index');
+        $this->view('home/index', $data);
         $this->view('templates/footer');
     }
 }
---
+++ app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+    class User_model{
+        private $nama = 'Farhan Yudhi Fatah';
+
+        public function getUser(){
+            return $this->nama;
+        }
+    }
